551da113db5c71b05fe2ed94ba0d9b912dccfe4c
Michiel de Jong <michiel@ilp.michielbdejong.com>
2017-02-22 13:23:07 +0000

    Use pm2

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 export CONNECTOR_LEDGERS=`node setup.js`
-SLIPPAGE=0 FX_SPREAD=0 CONNECTOR_AUTOLOAD_PEERS=true DEBUG=ilp-* npm start
+export SLIPPAGE=0
+export FX_SPREAD=0
+export CONNECTOR_AUTOLOAD_PEERS=true
+export DEBUG=ilp-*
+npm start
